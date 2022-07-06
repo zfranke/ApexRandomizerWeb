0973bf62c1d959ef8f85e1288d445b4de3983a94
zfranke <zfranke@live.com>
2022-07-06 15:25:14 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+/** 
+ * Project: Apex Web Randomizer
+ * Author: Zach Franke
+ * Date: 2022-07-06
+ * 
+ * Description: Apex Legends Web Randomizer
+ */
+
+const app = express();
+const port = process.env.PORT || 3000;
+
